97feb3978768bf786c936c6dbfd7082bc8e2e0a6
UGC454 <hms4436100@gmail.com>
2025-12-05 13:33:21 +0900

    REST server

---
+++ lec-06-prg-07-rest-server-v3.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+
+const myManager = {
+    database: {},
+
+    create: function(id, value) {
+        if (id in this.database) {
+            return { [id]: "None" };
+        } else {
+            this.database[id] = value;
+            return { [id]: this.database[id] };
+        }
+    },
+
+    read: function(id) {
+        if (id in this.database) {
+            return { [id]: this.database[id] };
+        } else {
+            return { [id]: "None" };
+        }
+    },
+
+    update: function(id, value) {
+        if (id in this.database) {
+            this.database[id] = value;
+            return { [id]: this.database[id] };
+        } else {
+            return { [id]: "None" };
+        }
+    },
+
+    delete: function(id) {
+        if (id in this.database) {
+            delete this.database[id];
+            return { [id]: "Removed" };
+        } else {
+            return { [id]: "None" };
+        }
+    }
+};
+
+app.listen(port, () => {
+    console.log(`REST API Server started on port ${port}`);
+});
